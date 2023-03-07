b3ebe0a2c0677824995bc1953a97c330b165259b
Samy El Hamass <samy.el-hamass@student.uibk.ac.at>
2023-03-07 22:42:26 +0100

    Video startable from website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import subprocess
 
 app = Flask(__name__)
@@ -10,5 +10,13 @@ def home():
 
 @app.route('/run_videoCreator')
 def run_script():
-  subprocess.run(['python', 'scripts/videoCreator.py'])
-  return 'Script executed successfully!'
+    query_string = request.query_string.decode()
+    print(query_string)
+    arg1 = request.args.get("arg1")
+    print("arg1: " + arg1)
+    arg2 = request.args.get("arg2")
+    print("arg2:  " + arg2)
+    arg3 = request.args.get("arg3")
+    print("arg3: " + arg3)
+    subprocess.run(['python', 'scripts/videoCreator/videoCreator.py', arg1, arg2, arg3])
+    return 'Script executed successfully!'
