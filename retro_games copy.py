f91af96ee9bb7958ec6c24ad7237c58fa85bfe20
Lukas Schändel <Mango>
2023-03-07 04:06:05 +0100

    Working Elevenlabs

--- retro_games copy.py
+++ retro_games copy.py
@@ -7,6 +7,7 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from mutagen.mp3 import MP3
 from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
 from elevenlabslib import ElevenLabsUser
+from pydub import AudioSegment
 import pydub
 import pydub.playback
 import io
@@ -32,22 +33,22 @@ themeStart = input()
 if not themeStart:
     themeStart= 0
 
-topic = "super mario"
-youtubeVideoLink = "https://www.youtube.com/watch?v=ia8bhFoqkVE"
-backgroundThemeLink = "https://www.youtube.com/watch?v=NTa6Xbzfq1U"
+topic = "Battlefield 4"
+youtubeVideoLink = "https://www.youtube.com/watch?v=cmYVGZ1w1y4"
+backgroundThemeLink = "https://www.youtube.com/watch?v=pUOibX2xV34"
 themeStart = 0
-videoStart = 35
+videoStart = 10
 
 ###### Get Text about the Game 
-API_SECRET_KEY_OPENAI = "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"
+API_SECRET_KEY_OPENAI = "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"
 
 openai.api_key = API_SECRET_KEY_OPENAI
 
-prompt = "Write a long Text for a video about " + topic
+prompt = "Write an 1 sentence short ironic review about " + topic + " saying the exact opposite of what the general opinion on the games biggest pain points is"
 
 model = "text-davinci-003"
 temperature = 0.9
-max_tokens = 200
+max_tokens = 30
 
 
 response = openai.Completion.create(prompt = prompt, model = model, temperature=temperature, max_tokens = max_tokens)
@@ -57,17 +58,16 @@ print(text)
 
 """
 ###### Text to Speech
-speech = gTTS(text = text, lang = "en-us", slow = False)
-speech.save("speech.mp3")
-
+#speech = gTTS(text = text, lang = "en-us", slow = False)
+#speech.save("speech.mp3")
 """
-      
+
 user = ElevenLabsUser("50053dae449db964a829c6dda45034ce") #fill in your api key as a string
-voice = user.get_voices_by_name("Rachel")[0]  #fill in the name of the voice you want to use. ex: "Rachel"
+voice = user.get_voices_by_name("Josh")[0]  #fill in the name of the voice you want to use. ex: "Rachel"
 voice_bytes = voice.generate_audio_bytes(text) #fill in what you want the ai to say as a string
 
 
-
+###### BG Audio
 
 audio = YouTube(backgroundThemeLink)
 audio_stream = audio.streams.filter(only_audio=True).first()
@@ -76,22 +76,25 @@ audio_stream.download(filename="backgroundAudio.mp3",)
 
 #Combine audio files 
 speech_audio = AudioSegment.from_file_using_temporary_files(io.BytesIO(voice_bytes))
+speech_audio.export("speech.mp3", format="mp3")
 background_audio = AudioSegment.from_file("backgroundAudio.mp3")
 
 
 # Get the duration of the speech audio in milliseconds
-speech_duration = len(speech_audio)
+audioLength = MP3("speech.mp3")
+speech_duration = audioLength.info.length
 
 # Cut the background audio to the same duration as the speech audio
 background_audio = background_audio[int(themeStart*1000):]
 background_audio = background_audio[:speech_duration]
 
 # Adjust the volume of the speech audio
-speech_audio = speech_audio + 5  # increase volume by 25 dB
+speech_audio = speech_audio
 background_audio = background_audio - 10
 
 # Overlay the speech audio onto the background audio
-combined_audio = background_audio.overlay(speech_audio)
+speech_audio_to_combine = AudioSegment.from_mp3("speech.mp3")
+combined_audio = background_audio.overlay(speech_audio_to_combine)
 
 # Export the combined audio as an mp3 file
 combined_audio.export("combinedAudio.mp3", format="mp3")
@@ -102,7 +105,7 @@ combined_audio.export("combinedAudio.mp3", format="mp3")
 yt = YouTube(youtubeVideoLink)
 stream = yt.streams.get_highest_resolution()
 stream.download(filename="video.mp4")
-audioLength = MP3("speech.mp3")
-cutVideo = VideoFileClip("video.mp4").subclip(int(videoStart), int(videoStart) + audioLength.info.length).without_audio()
+#audioLength = MP3("speech.mp3")
+cutVideo = VideoFileClip("video.mp4").subclip(int(videoStart), int(videoStart) + speech_duration).without_audio()
 cutVideo = cutVideo.set_audio(AudioFileClip("combinedAudio.mp3"))
 cutVideo.write_videofile("cutVideo.mp4")
---
+++ test.py
@@ -0,0 +1,106 @@
+import openai
+from gtts import gTTS
+from googleapiclient.discovery import build
+from pytube import YouTube
+from pydub import AudioSegment
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from mutagen.mp3 import MP3
+from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
+from elevenlabslib import ElevenLabsUser
+from pydub import AudioSegment
+import pydub
+import pydub.playback
+import io
+
+#Get input
+print("Enter Topic: ")
+topic = input()
+print("Enter Youtube Video Link:")
+youtubeVideoLink = input()
+print("Enter Video Start in seconds: ")
+videoStart = input()
+if not videoStart:
+    videoStart = 0
+print("Enter Video End in seconds: ")
+videoEnd = input()
+if not videoEnd:
+    videoEnd = 0
+print("Enter Background Theme/music: ")
+backgroundThemeLink = input()
+print("Theme Start in seconds: ")
+themeStart = input()
+if not themeStart:
+    themeStart= 0
+
+topic = "Battlefield 4"
+youtubeVideoLink = "https://www.youtube.com/watch?v=cmYVGZ1w1y4"
+backgroundThemeLink = "https://www.youtube.com/watch?v=pUOibX2xV34"
+themeStart = 0
+videoStart = 40
+
+###### Get Text about the Game 
+API_SECRET_KEY_OPENAI = "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"
+
+openai.api_key = API_SECRET_KEY_OPENAI
+
+prompt = "Write a long ironic review about " + topic + " in the style of SsethTzeentach mocking the developers and making fun of community members which liked the game nevertheless using curse words"
+
+model = "text-davinci-003"
+temperature = 0.9
+max_tokens = 250
+
+
+response = openai.Completion.create(prompt = prompt, model = model, temperature=temperature, max_tokens = max_tokens)
+
+text = response["choices"][0]["text"]
+print(text)
+
+###### Text to Speech
+#speech = gTTS(text = text, lang = "en-us", slow = False)
+#speech.save("speech.mp3")
+
+user = ElevenLabsUser("50053dae449db964a829c6dda45034ce") #fill in your api key as a string
+voice = user.get_voices_by_name("Josh")[0]  #fill in the name of the voice you want to use. ex: "Rachel"
+voice_bytes = voice.generate_audio_bytes(text) #fill in what you want the ai to say as a string
+
+
+
+
+audio = YouTube(backgroundThemeLink)
+audio_stream = audio.streams.filter(only_audio=True).first()
+audio_stream.download(filename="backgroundAudio.mp3",)
+
+
+#Combine audio files 
+speech_audio = AudioSegment.from_file_using_temporary_files(io.BytesIO(voice_bytes))
+speech_audio.export("speech.mp3", format="mp3")
+background_audio = AudioSegment.from_file("backgroundAudio.mp3")
+
+
+# Get the duration of the speech audio in milliseconds
+speech_duration = len(speech_audio)
+
+# Cut the background audio to the same duration as the speech audio
+background_audio = background_audio[int(themeStart*1000):]
+background_audio = background_audio[:speech_duration]
+
+# Adjust the volume of the speech audio
+speech_audio = speech_audio + 5  # increase volume by 25 dB
+background_audio = background_audio - 10
+
+# Overlay the speech audio onto the background audio
+combined_audio = background_audio.overlay(speech_audio)
+
+# Export the combined audio as an mp3 file
+combined_audio.export("combinedAudio.mp3", format="mp3")
+
+###### Get Youtube Video
+
+# Create a YouTube object and get the highest resolution stream
+yt = YouTube(youtubeVideoLink)
+stream = yt.streams.get_highest_resolution()
+stream.download(filename="video.mp4")
+audioLength = MP3("speech.mp3")
+cutVideo = VideoFileClip("video.mp4").subclip(int(videoStart), int(videoStart) + audioLength.info.length).without_audio()
+cutVideo = cutVideo.set_audio(AudioFileClip("combinedAudio.mp3"))
+cutVideo.write_videofile("cutVideo.mp4")
